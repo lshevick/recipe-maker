22c8decf2ffc41a483e7fd9c52d1a0a940d53e3a
Levi Shevick <levidshevick@gmail.com>
2022-06-27 13:56:06 -0400

    adds frontend directory

--- conf/settings.py
+++ conf/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'api.apps.ApiConfig',
     'ingredients.apps.IngredientsConfig',
     'recipes.apps.RecipesConfig',
+    'frontend.apps.FrontendConfig',
 ]
 
 
@@ -140,11 +141,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend/static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'frontend/static/build/static'),)
 
 
 SITE_ID = 1
